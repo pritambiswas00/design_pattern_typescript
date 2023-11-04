53431874b3f85de882c5c8f6f761c93ffa2a85f9
Pritam Biswas <pritambiswas060@gmail.com>
2023-11-04 16:42:43 +0530

    Iterate Pattern implementation.

--- README.md
+++ README.md
@@ -16,6 +16,7 @@ This project was created using `bun init` in bun v1.0.7. [Bun](https://bun.sh) i
 
 ## Overview
 Provide a brief overview of the design pattern, explaining its purpose and when to use it.
+Here, the implementation about the Iterator Pattern.
 
 ## Implementation Details
 Explain the core concepts and components of the design pattern. Provide code snippets and examples where necessary.
--- index.ts
+++ index.ts
@@ -1,1 +1,47 @@
-console.log("Hello via Bun!");
+//This is the pattern mainly used to iterate some data structure like array, queue, stack.
+
+class Iterate<T>{
+    private data: T[]
+    constructor(data: T[]) {
+        this.data = data;
+    }
+    [Symbol.iterator]() {
+        let i = 0;
+        let iterate = this;
+        return {
+            next: function () {
+                if (i < iterate.data.length) {
+                    return {
+                        done: false,
+                        value: iterate.data[i++]
+                    }
+                } else {
+                    return {
+                        done: true,
+                        value: undefined
+                    }
+                }
+            }
+        }
+    }
+}
+
+const some_data = [1, "two", { name: "John" }, [4, 5, 6]];
+let test_iterator = new Iterate(some_data);
+for (let i of test_iterator) {
+    console.log(i, "Data")
+}
+//Suppose we have this data 
+const weirdData = [
+    "🦄",
+    { name: "Custom Object", age: 42 },
+    "\u266A",
+    () => console.log("Hello from a function!"),
+    NaN,
+    undefined,
+    Symbol("Symbol"),
+];
+
+for (const item of new Iterate(weirdData)) {
+    console.log(item, "Iterate Data")
+}
